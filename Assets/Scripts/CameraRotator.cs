fb085384a510d384a32c418a8d97d80e716b442b
DESKTOP-C5KNARJ\mh122354 <michaelhabib1868@gmail.com>
2016-08-25 22:00:12 -0700

    Create Camera Gimble

--- Assets/Scripts/CameraRotator.cs
+++ Assets/Scripts/CameraRotator.cs
@@ -10,14 +10,18 @@ public class CameraRotator : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
+
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
         mouseX = Input.GetAxis("Mouse X")*rotationSpeed;
-        mouseY = Input.GetAxis("Mouse Y")+rotationSpeed;
-        transform.rotation = Quaternion.Euler(-mouseY,mouseX,0) * transform.rotation;
+        mouseY = Input.GetAxis("Mouse Y")*rotationSpeed;
+        transform.localRotation = Quaternion.Euler(0, mouseX, 0) * transform.localRotation;
+        Camera camera = GetComponentInChildren<Camera>();
+        camera.transform.localRotation = Quaternion.Euler(-mouseY, 0, 0) * camera.transform.localRotation;
+
 	
 	}
 }
