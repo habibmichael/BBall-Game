8ff1d149a6a7ae4f911dc0979d07c1ed819f3a27
DESKTOP-C5KNARJ\mh122354 <michaelhabib1868@gmail.com>
2016-08-26 04:56:22 -0700

    Add scoring capability

---
+++ Assets/Scripts/ScoreDetector.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreDetector : MonoBehaviour
+{
+    public int scorePerHit=1;
+    ScoreKeeper scoreKeeper;
+
+    void OnCollisionEnter(Collision collision )
+    {
+        scoreKeeper=FindObjectOfType<ScoreKeeper>();
+        scoreKeeper.incrementScore(scorePerHit);
+    }
+
+}
---
+++ Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreKeeper : MonoBehaviour
+{
+
+    int score;
+
+    // Use this for initialization
+    void Start ()
+    {
+
+        score = 0;
+    }
+
+
+    public void incrementScore (int scorePerHit)
+    {
+        score += scorePerHit;
+        print("Score: " + score);
+    }
+}
