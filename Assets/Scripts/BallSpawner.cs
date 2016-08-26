c8ada58b591222aeab0593295c39a00f946a8d23
DESKTOP-C5KNARJ\mh122354 <michaelhabib1868@gmail.com>
2016-08-25 21:17:19 -0700

    Create ball spawner , set camera to mouse input

---
+++ Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallSpawner : MonoBehaviour {
+
+    public GameObject ballPrefab;
+    GameObject ballInstance;
+    Rigidbody rb;
+    float speed=5f;
+
+	// Use this for initialization
+	void Start () {
+
+      
+	
+	}
+
+    // Update is called once per frame
+    void Update () {
+        /*
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+               GameObject instance = Instantiate(ballPrefab);
+               Rigidbody rb =  instance.GetComponent<Rigidbody>();
+
+            }
+            */
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            ballInstance = Instantiate(ballPrefab);
+            rb = ballInstance.GetComponent<Rigidbody>();
+            rb.velocity = Vector3.up *speed;
+
+         }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+
+        {
+            ballInstance = Instantiate(ballPrefab);
+            rb = ballInstance.GetComponent<Rigidbody>();
+            rb.velocity = Vector3.down *speed;
+
+        } else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            ballInstance = Instantiate(ballPrefab);
+            rb = ballInstance.GetComponent<Rigidbody>();
+            rb.velocity = Vector3.left *speed;
+
+        }  else if (Input.GetKeyDown(KeyCode.RightArrow))   {
+
+            ballInstance = Instantiate(ballPrefab);
+            rb = ballInstance.GetComponent<Rigidbody>();
+            rb.velocity = Vector3.right *speed;
+
+        }
+	}
+}
---
+++ Assets/Scripts/CameraRotator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotator : MonoBehaviour {
+
+    float mouseX;
+    float mouseY;
+    float rotationSpeed = 5f;
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        mouseX = Input.GetAxis("Mouse X")*rotationSpeed;
+        mouseY = Input.GetAxis("Mouse Y")+rotationSpeed;
+        transform.rotation = Quaternion.Euler(-mouseY,mouseX,0) * transform.rotation;
+	
+	}
+}
