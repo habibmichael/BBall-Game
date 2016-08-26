0d06edb070fc4776b7831a9b8049e96454de7bef
DESKTOP-C5KNARJ\mh122354 <michaelhabib1868@gmail.com>
2016-08-25 22:29:14 -0700

    Create Ball Launcher Class

---
+++ Assets/Scripts/BallLauncher.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallLauncher : MonoBehaviour {
+
+    public float ballSpeed = 5f;
+   public  GameObject ballPrefab;
+    GameObject ballInstance;
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Look for button click or left-ctrl
+        if (Input.GetButtonDown("Fire1"))
+        {
+            ballInstance = Instantiate(ballPrefab);
+            ballInstance.transform.position = transform.position;
+            rb = ballInstance.GetComponent<Rigidbody>();
+            rb.velocity = Vector3.left * ballSpeed;
+        }
+	}
+}
